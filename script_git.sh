7c74302bb4c74edbcb1e35ebc4d09e22fdf9d052
Victor <victor.stf@gmail.com>
2020-03-16 21:16:35 +0000

    first commit

---
+++ script_git.sh
@@ -0,0 +1,6 @@
+echo "# k8s-vagrant-ansible" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/victorpaulo/k8s-vagrant-ansible.git
+git push -u origin master
